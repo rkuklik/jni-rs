f438ff112ec733d5902507045e3e2d97c3a0e86e
Kuklík Radek <kuklik.radek@kytkos.cz>
2026-03-20 19:18:52 +0100

    jvmti: initialize crate

---
+++ crates/jvmti/src/lib.rs
@@ -0,0 +1,17 @@
+//#![warn(missing_docs)]
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
+#![warn(clippy::alloc_instead_of_core)]
+#![warn(clippy::std_instead_of_core)]
+#![warn(clippy::std_instead_of_alloc)]
+#![deny(missing_debug_implementations)]
+
+//! # Safe JVMTI Bindings in Rust
+//!
+//! This is a placeholder for now, development happens at:
+//! [https://github.com/rkuklik/jni-rs]
+
+extern crate alloc;
+extern crate core;
+
+pub use jni;
+pub use jvmti_sys as sys;
