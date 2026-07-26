89ad56da0fd69bd90ab8f0577200cecbcac27ba7
Kuklík Radek <kuklik.radek@kytkos.cz>
2026-07-26 18:30:57 +0200

    jvmti: use only function for `Agent_On*` arguments

--- crates/jvmti/src/agent.rs
+++ crates/jvmti/src/agent.rs
@@ -1,100 +1,85 @@
-use core::num::NonZero;
+use core::fmt;
 
-use jni::strings::JNIStr;
-use jni::vm::JavaVM;
+use jni_sys::JNI_ERR;
+use jni_sys::JNI_OK;
 use jni_sys::jint;
 
-type StartResult = Result<(), NonZero<jint>>;
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ResultCode(jint);
 
-pub trait AgentLoad {
-    fn load(self, vm: JavaVM, options: &JNIStr) -> StartResult;
-}
+impl ResultCode {
+    pub const SUCCESS: Self = Self(JNI_OK);
+    pub const FAILURE: Self = Self(JNI_ERR);
 
-pub trait AgentAttach {
-    fn attach(self, vm: JavaVM, options: &JNIStr) -> StartResult;
-}
+    pub const fn new(value: jint) -> Self {
+        Self(value)
+    }
 
-pub trait AgentUnload {
-    fn unload(self, vm: JavaVM);
-}
-
-pub trait IntoStartResult {
     #[doc(hidden)]
-    fn resolve(self) -> StartResult;
-}
-
-impl IntoStartResult for () {
-    fn resolve(self) -> StartResult {
-        Ok(())
+    pub const fn raw(self) -> jint {
+        self.0
     }
 }
 
-impl IntoStartResult for jint {
-    fn resolve(self) -> StartResult {
-        match NonZero::new(self) {
-            Some(code) => Err(code),
-            None => Ok(()),
+impl From<()> for ResultCode {
+    fn from((): ()) -> Self {
+        Self::SUCCESS
+    }
+}
+
+impl From<jint> for ResultCode {
+    fn from(value: jint) -> Self {
+        Self(value)
+    }
+}
+
+impl<T, E> From<Result<T, E>> for ResultCode
+where
+    T: Into<ResultCode>,
+    E: fmt::Debug,
+{
+    fn from(value: Result<T, E>) -> Self {
+        match value {
+            Ok(res) => res.into(),
+            Err(err) => panic!("error starting agent {err:?}"),
         }
-    }
-}
-
-impl IntoStartResult for StartResult {
-    fn resolve(self) -> StartResult {
-        self
-    }
-}
-
-impl<T: IntoStartResult, F: FnOnce(JavaVM, &JNIStr) -> T> AgentLoad for F {
-    fn load(self, vm: JavaVM, options: &JNIStr) -> Result<(), NonZero<jint>> {
-        self(vm, options).resolve()
-    }
-}
-
-impl<T: IntoStartResult, F: FnOnce(JavaVM, &JNIStr) -> T> AgentAttach for F {
-    fn attach(self, vm: JavaVM, options: &JNIStr) -> Result<(), NonZero<jint>> {
-        self(vm, options).resolve()
-    }
-}
-
-impl<F: FnOnce(JavaVM)> AgentUnload for F {
-    fn unload(self, vm: JavaVM) {
-        self(vm)
     }
 }
 
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __agent_start {
-    // this blight works around: implementation of `core::ops::FnOnce` is not general enough
-    // See: https://users.rust-lang.org/t/i-dont-understand-this-lifetime-error/137426
-    ($name:ident, $call:path, $($move:ident)? |$($arg:tt $(: $type:ty)?),+ $(,)?| $($body:tt)+) => {
-        $crate::__agent_start!($name, $call, {
-            // inference breaks on closures with higher-kinded types but nudge via identity function helps
-            const fn infer<T, F>(f: F) -> F
-            where
-                T: $crate::agent::StartResult,
-                F: ::core::ops::FnOnce($crate::jni::vm::JavaVM, &$crate::jni::strings::JNIStr) -> T,
-            {
-                f
-            }
-            infer($($move)? |$($arg $(:$type)?,)+| $($body)+)
-        });
-    };
-    ($name:ident, $call:path, $agent:expr) => {
+    ($name:ident, $func:expr) => {
         #[unsafe(no_mangle)]
         unsafe extern "C" fn $name(
             vm: *mut $crate::jni::sys::JavaVM,
             options: *mut ::core::ffi::c_char,
             reserved: *mut ::core::ffi::c_void,
         ) -> $crate::jni::sys::jint {
-            _ = reserved;
-            let jvm = unsafe { $crate::jni::vm::JavaVM::from_raw(vm) };
-            let arg = unsafe { $crate::jni::strings::JNIStr::from_ptr(options) };
-            let res = $call($agent, jvm, arg);
-            match res {
-                Ok(()) => 0,
-                Err(code) => code.get(),
+            use ::core::convert::Into;
+            use ::core::ops::FnOnce;
+
+            use $crate::agent::ResultCode;
+            use $crate::jni::strings::JNIStr;
+            use $crate::jni::vm::JavaVM;
+
+            // inference sometimes breaks on closures with higher-kinded types but nudge via identity
+            // helps circumvent error: implementation of `core::ops::FnOnce` is not general enough
+            // see: https://users.rust-lang.org/t/i-dont-understand-this-lifetime-error/137426
+            const fn infer<T, F>(f: F) -> F
+            where
+                T: Into<ResultCode>,
+                F: FnOnce(JavaVM, &JNIStr) -> T,
+            {
+                f
             }
+
+            _ = reserved;
+            let jvm = unsafe { JavaVM::from_raw(vm) };
+            let arg = unsafe { JNIStr::from_ptr(options) };
+            let fun = infer($func);
+            let res = fun(jvm, arg);
+            Into::<ResultCode>::into(res).raw()
         }
 
         const _: $crate::sys::$name = $name;
@@ -103,25 +88,35 @@ macro_rules! __agent_start {
 
 #[macro_export]
 macro_rules! onload {
-    ($($agent:tt)*) => {
-        $crate::__agent_start!(Agent_OnLoad, $crate::agent::AgentLoad::load, $($agent)*);
+    ($func:expr) => {
+        $crate::__agent_start!(Agent_OnLoad, $func);
     };
 }
 
 #[macro_export]
 macro_rules! onattach {
-    ($($agent:tt)*) => {
-        $crate::__agent_start!(Agent_OnAttach, $crate::agent::AgentAttach::attach, $($agent)*);
+    ($func:expr) => {
+        $crate::__agent_start!(Agent_OnAttach, $func);
     };
 }
 
 #[macro_export]
 macro_rules! onunload {
-    ($agent:expr) => {
+    ($func:expr) => {
         #[unsafe(no_mangle)]
         unsafe extern "C" fn Agent_OnUnload(vm: *mut $crate::jni::sys::JavaVM) {
-            let jvm = unsafe { $crate::jni::vm::JavaVM::from_raw(vm) };
-            $crate::agent::AgentUnload::unload($agent, jvm)
+            use $crate::jni::vm::JavaVM;
+
+            const fn infer<F>(f: F) -> F
+            where
+                F: ::core::ops::FnOnce(JavaVM),
+            {
+                f
+            }
+
+            let jvm = unsafe { JavaVM::from_raw(vm) };
+            let fun = infer($func);
+            fun(jvm)
         }
 
         const _: $crate::sys::Agent_OnUnload = Agent_OnUnload;
